8fed6dc2a1dfca91039b8b5db733c01d9c970567
austi <austi@HP_AUSTIN>
2024-02-08 13:00:25 -0600

    Add project files.

---
+++ mom2/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim num1, num2 As Integer
+        num1 = Val(TextBox1.Text)
+        num2 = Val(TextBox2.Text)
+        If num1 > num2 Then
+            MessageBox.Show("El numero mayor es " & num1 & " y el numero menor es " & num2)
+        Else
+            If num2 > num1 Then
+                MessageBox.Show("El numero mayor es " & num2 & " y el numero menor es " & num1)
+            Else
+                MessageBox.Show("Los numeros son iguales")
+            End If
+        End If
+
+    End Sub
+End Class
